21d42191b8129d055ffeb9cabd92f1487b06cf6a
FelipeAmrl <felipeamaral.bio@gmail.com>
2023-06-21 16:04:23 -0300

    ft: create authContext and authorization hook

---
+++ src/hooks/useAuth.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/prop-types */
+import { createContext, useState, useContext } from "react";
+
+import { api } from "../services/api";
+
+export const AuthContext = createContext({});
+
+function AuthProvider({children})
+{
+    const [ data, setData ] = useState({});
+
+    async function signIn({ email, password })
+    {
+        try
+        {
+            const response = await api.post("/sessions", { email, password });
+            const { user, token } = response.data;
+
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+            setData({ user, token });
+        }
+        catch(error)
+        {
+            if(error.response)
+                alert(error.response.data.message);
+            else
+                alert('Unable to login!');
+        }
+    }
+
+    return (
+        <AuthContext.Provider 
+            value={{ signIn, user: data.user}}
+        >
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+function useAuth()
+{
+    const context = useContext(AuthContext);
+    return context;
+}
+
+export { AuthProvider, useAuth };
--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,15 @@ import  { ThemeProvider } from 'styled-components';
 
 import { Routes } from './routes';
 
+import { AuthProvider } from './hooks/useAuth';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
         <GlobalStyles />
-        <Routes />
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
