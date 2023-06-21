ff875ee485765cbee31b3a3cef3898e308f4581e
FelipeAmrl <felipeamaral.bio@gmail.com>
2023-06-21 16:47:33 -0300

    ft: add user signIn functionality

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,14 +1,26 @@
-import { Container, Form, Background } from './styles';
-
-import { FiMail, FiLock } from 'react-icons/fi';
-
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
+import { Container, Form, Background } from './styles';
+
+import { useState } from 'react';
+import { FiMail, FiLock } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+
+import { useAuth } from '../../hooks/useAuth';
 
 export function SignIn()
 {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const { signIn } = useAuth();
+
+    function handleSignIn()
+    {
+        signIn({email, password});
+    }
+
     return (
         <Container>
             <Form>
@@ -18,10 +30,24 @@ export function SignIn()
 
                 <h2>Sign In</h2>
 
-                <Input type="email" placeholder="E-mail" icon={FiMail}/>
-                <Input type="password" placeholder="Password" icon={FiLock}/>
+                <Input 
+                    type="email" 
+                    placeholder="E-mail" 
+                    icon={FiMail}
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <Input 
+                    type="password" 
+                    placeholder="Password" 
+                    icon={FiLock}
+                    onChange={e => setPassword(e.target.value)}
 
-                <Button title="Login"/>
+                />
+
+                <Button 
+                    title="Login"
+                    onClick={handleSignIn}
+                />
 
                 <Link to="/register">
                     Create account
